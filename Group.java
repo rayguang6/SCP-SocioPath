527cc993af263d817c2b771e96276170337d7045
lemonmel <76807277+lemonmel@users.noreply.github.com>
2021-06-11 20:28:55 +0800

    Update Group.java

--- Group.java
+++ Group.java
@@ -12,7 +12,8 @@ import java.util.Arrays;
  *
  * @author doublechin
  */
-public class Group<T extends Comparable<T>, N extends Comparable<N>> {
+public class Group<T extends Comparable<T>, N extends Comparable<N>> implements Cloneable {
+
     Student<T, N> head;
     int size;
 
@@ -41,24 +42,21 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
     }
 
     public boolean addEdge(T source, T destination, int rep, boolean friend) {
-        if (head == null) {
-            return false;
-        }
-        if (!hasStudent(source) || !hasStudent(destination)) {
-            return false;
-        }
+        //no condition checking required as alredy checked in previous event
         Student<T, N> sourceV = head;
         while (sourceV != null) {
             if (sourceV.vertexInfo.equals(source)) {
                 Student<T, N> destV = head;
                 while (destV != null) {
                     if (destV.vertexInfo.equals(destination)) {
-                        Edge<T, N> currentFriend = sourceV.firstFriend;
-                        Edge<T, N> newFriend = new Edge<>(destV, rep, currentFriend);
-                        sourceV.firstFriend = newFriend;
+                        Edge<T, N> firstRelative = sourceV.relativeRep;
+                        Edge<T, N> newRelative = new Edge<>(destV, rep, firstRelative);
+                        sourceV.relativeRep = newRelative;
                         sourceV.outdeg++;
                         destV.indeg++;
-                        if(friend) sourceV.friendList.add(destination);
+                        if (friend) {
+                            sourceV.friendList.add(destination);
+                        }
                         return true;
                     }
                     destV = destV.nextVertex;
@@ -68,8 +66,8 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
         }
         return false;
     }
-    
-    public boolean addUndirectedEdge(T source, T destination){
+
+    public boolean addUndirectedEdge(T source, T destination) {
         return addEdge(source, destination, 0, true) && addEdge(destination, source, 0, true);
     }
 
@@ -92,15 +90,15 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
         }
         return false;
     }
-    
-    public void printAllStudentProfile(){
-        Student<T,N> temp=head;
-        while(temp!=null){
+
+    public void printAllStudentProfile() {
+        Student<T, N> temp = head;
+        while (temp != null) {
             System.out.println(temp.toString());
-            temp=temp.nextVertex;
+            temp = temp.nextVertex;
         }
     }
-    
+
     public boolean hasStudent(T v) {
         if (head == null) {
             return false;
@@ -153,52 +151,58 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
 
     public void printEdges() { //print friends of all with rep point
         Student<T, N> temp = head;
-        while(temp!=null){
-            Edge<T,N> [] list= new Edge[temp.outdeg]; //creating array to sort
-            Edge<T, N> currentEdge = temp.firstFriend;
-            int i=0;
-            while(currentEdge!=null){
-                list[i]=currentEdge;
-                currentEdge=currentEdge.nextEdge;
+        while (temp != null) {
+            Edge<T, N>[] list = new Edge[temp.outdeg]; //creating array to sort
+            Edge<T, N> currentEdge = temp.relativeRep;
+            int i = 0;
+            while (currentEdge != null) {
+                list[i] = currentEdge;
+                currentEdge = currentEdge.nextEdge;
                 i++;
             }
             Arrays.sort(list); //sort according their rep point
-            System.out.print("#"+temp.vertexInfo+" : ");
-            for(int j=0; j<list.length; j++){
+            System.out.print("#" + temp.vertexInfo + " : ");
+            for (int j = 0; j < list.length; j++) {
                 System.out.print("[");
-                if(temp.friendList.contains(list[j].toVertex.vertexInfo)) System.out.print("Friend: ");
-                else System.out.print("Stranger: ");
-                System.out.print(list[j].toVertex.vertexInfo + ": "+list[j].rep+"p]");
+                if (temp.friendList.contains(list[j].toVertex.vertexInfo)) {
+                    System.out.print("Friend: ");
+                } else {
+                    System.out.print("Stranger: ");
+                }
+                System.out.print(list[j].toVertex.vertexInfo + ": " + list[j].rep + "p]");
             }
             System.out.println("");
-            temp=temp.nextVertex;
+            temp = temp.nextVertex;
         }
     }
-    
+
     public void printEdges(T v) { //print friends of one with rep point
         Student<T, N> temp = head;
-        while(temp!=null){
-            if(temp.vertexInfo.equals(v)){
-                Edge<T,N> [] list= new Edge[temp.outdeg];
-                Edge<T, N> currentEdge = temp.firstFriend;
-                int i=0;
-                while(currentEdge!=null){
-                    list[i]=currentEdge;
-                    currentEdge=currentEdge.nextEdge;
+        while (temp != null) {
+            if (temp.vertexInfo.equals(v)) {
+                Edge<T, N>[] list = new Edge[temp.outdeg];
+                Edge<T, N> currentEdge = temp.relativeRep;
+                int i = 0;
+                while (currentEdge != null) {
+                    list[i] = currentEdge;
+                    currentEdge = currentEdge.nextEdge;
                     i++;
                 }
                 Arrays.sort(list);
-                System.out.print("#"+temp.vertexInfo+" : ");
-                for(int j=0; j<list.length; j++){
+                System.out.print("#" + temp.vertexInfo + " : ");
+                for (int j = 0; j < list.length; j++) {
                     System.out.print("[");
-                    if(temp.friendList.contains(list[j].toVertex.vertexInfo)) System.out.print("Friend: ");
-                    else System.out.print("Stranger: ");
-                    System.out.print(list[j].toVertex.vertexInfo + ": "+list[j].rep+"p]");
+                    if (temp.friendList.contains(list[j].toVertex.vertexInfo)) {
+                        System.out.print("Friend: ");
+                    } else {
+                        System.out.print("Stranger: ");
+                    }
+                    System.out.print(list[j].toVertex.vertexInfo + ": " + list[j].rep + "p]");
                 }
                 System.out.println("");
                 break;
             }
-            temp=temp.nextVertex;
+            temp = temp.nextVertex;
         }
     }
 
@@ -221,15 +225,14 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
         Student<T, N> temp = head;
         while (temp != null) {
             if (temp.vertexInfo.compareTo(v) == 0) {
-                // Reached vertex, look for destination now
                 return temp.friendList;
             }
             temp = temp.nextVertex;
         }
         return list;
     }
-    
-    public void printFriends(){ //print friends of all
+
+    public void printFriends() { //print friends of all
         Student<T, N> temp = head;
         while (temp != null) {
             System.out.print("# " + temp.vertexInfo + " : ");
@@ -237,8 +240,8 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
             temp = temp.nextVertex;
         }
     }
-    
-    public boolean teachStrager(T mentor, T mentee, boolean good){ //feature 1
+
+    public boolean teachStrager(T mentor, T mentee, boolean good) { //feature 1
         if (head == null) {
             return false;
         }
@@ -270,8 +273,8 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
         }
         return true;
     }
-    
-    public boolean chitchat(T talker, T listener, T rumors, boolean good){ //feature 2
+
+    public boolean chitchat(T talker, T listener, T rumors, boolean good) { //feature 2
         if (head == null) {
             return false;
         }
@@ -297,109 +300,106 @@ public class Group<T extends Comparable<T>, N extends Comparable<N>> {
             }
         }
     }
-    
-    public void haveLunch(int[] student, T me， int day){ // updated feature 3: PARALLEL FARMING
-        Student<T, N>[] ary=new Student[student.length];
-        Student<T, N> ME=new Student<>();
+
+    public void haveLunch(int[] student, T me, int day) { //feature 3
+        Student<T, N>[] ary = new Student[student.length];
+        Student<T, N> ME = new Student<>();
         //inseting students into a list
-        for(int i=0; i<student.length; i++){
+        for (int i = 0; i < student.length; i++) {
             Student<T, N> sourceV = head;
-            while(sourceV!=null){
+            while (sourceV != null) {
                 if (sourceV.vertexInfo.equals(student[i])) {
-                    //calculating average lunch period and end time
                     while(sourceV.lunchPeriod.size()<day){
                         sourceV.generateTime();
                     }
-                    sourceV.calculateAverage();
-                    ary[i]=sourceV;
-                }else if(sourceV.vertexInfo.equals(me)){
+                    sourceV.calculateAverage(day);
+                    ary[i] = sourceV;
+                } else if (sourceV.vertexInfo.equals(me) && i==0) {
                     while(sourceV.lunchPeriod.size()<day){
                         sourceV.generateTime();
                     }
-                    sourceV.calculateAverage();
-                    ME=sourceV;
+                    sourceV.calculateAverage(day);
+                    ME = sourceV;
                 }
-                sourceV=sourceV.nextVertex;
+                sourceV = sourceV.nextVertex;
             }
         }
-        //sort accroding priority, who will finished their lunch earlier will have higher priority
+        //sort accroding priority, less time used will be priotized, first observed has least time
         Arrays.sort(ary);
-        //ArrayList indicates the schedule plan to have lunch with who
+        //ArrayList indicates the schedule plan with who, start time and end time
         ArrayList<T> people = new ArrayList<>();
-        //declare an array with minutes value of the observant each index indicates each minute
-        int[] time_slot=new int[ME.average_lunchPeriod];
+        //declare an array with minutes value of the observant
+        int[] time_slot = new int[ME.average_lunchPeriod];
         //calculate my minute value of start time and end time
-        int my_start_min=calcMin(ME.average_lunchStart);
-        int my_end_min= calcMin(ME.end_time);
+        int my_start_min = calcMin(ME.average_lunchStart);
+        int my_end_min = calcMin(ME.end_time);
         //print my eating time
-        System.out.println("My eating time : "+ME.average_lunchStart+" "+ME.average_lunchPeriod+" "+ME.end_time);
+        System.out.println("My eating time : " + ME.average_lunchStart + " " + ME.average_lunchPeriod + " " + ME.end_time);
         //go for loop to check every individual time of eating 
         System.out.println("[people]starting time--period---end time");
-        for(int i=0; i<ary.length; i++){
+        for (int i = 0; i < ary.length; i++) {
             //calculate their minute value of eating time
-            int start_min=calcMin(ary[i].average_lunchStart);
-            int end_min=calcMin(ary[i].end_time);
-            //check is there intersection on observant and observer time of eating
-            /* CONDITION: 
-            his_start---mystart----his_end----myend 
-            mystart---his_start---myend---his_end 
-            his_start--mystart---myend---his_end 
-            */
-            if(end_min >= my_start_min && end_min <= my_end_min 
+            int start_min = calcMin(ary[i].average_lunchStart);
+            int end_min = calcMin(ary[i].end_time);
+            //to display value for checking purpose only
+            System.out.println("[" + ary[i].vertexInfo + "]" + ary[i].average_lunchStart + " " + ary[i].average_lunchPeriod + " " + ary[i].end_time);
+            //calculating slot in minutes value 
+            //check is there intersection on observant and observer time of eating, if no, straight print result
+            //his_start---mystart----his_end----myend 
+            //mystart---his_start---myend---his_end 
+            //his_start--mystart---myend---his_end
+            if (end_min >= my_start_min && end_min <= my_end_min
                     || start_min >= my_start_min && start_min <= my_end_min
-                    || start_min<=my_start_min && end_min>=my_end_min){
+                    || start_min <= my_start_min && end_min >= my_end_min) {
                 //compare with array, to check whether the table is full or not
-                boolean full=false;
-                for(int ss=0; ss<ME.average_lunchPeriod; ss++){
-                    if(time_slot[ss]>=3){
-                        full=true;
+                boolean full = false;
+                for (int ss = 0; ss < ME.average_lunchPeriod; ss++) {
+                    if (time_slot[ss] >= 3) {
+                        full = true;
                         break;
                     }
                 }
-                //if the table is not full with 3 people yet, I can eat with them
-                if(!full){
+                //if the table is not full with 3 people yet, they can eat with them
+                if (!full) {
                     people.add(ary[i].vertexInfo);
-                    /*
-                    the value on each index of time_slot indicates the number of people on table on that minute, +1 to add people to the table
-                    eg. my starting time is 1312, index 0 indicates the first minute of my eating time (1312-1313) 
-                    if student start before my eating time, addition start from index 0 until the time he finished his lunch
-                    if student start after my eating time, subtraction needed to compare his start eating time with my starting time
-                    */
-                    int temp=0;
-                    if(start_min>=my_start_min){
-                        temp=start_min-my_start_min;
+                    int temp = 0;
+                    if (start_min >= my_start_min) {
+                        temp = start_min - my_start_min;
                     }
-                    for(int s=temp; s<end_min-my_start_min; s++){
-                        if(s>=time_slot.length){
+                    for (int s = temp; s < end_min - my_start_min; s++) {
+                        if (s >= time_slot.length) {
                             break;
                         }
-                        time_slot[s]=time_slot[s]+1;
+                        time_slot[s] = time_slot[s] + 1;
                     }
+                    for (int v = 0; v < time_slot.length; v++) {
+                        if (v >= temp && v < end_min - my_start_min) {
+                            System.out.print(ary[i].vertexInfo);
+                        } else {
+                            System.out.print(0);
+                        }
+                    }
+                    System.out.println("");
                 }
             }
-            //to display value for checking purpose only
-            System.out.println("["+ary[i].vertexInfo+"]"+ary[i].average_lunchStart+" "+ary[i].average_lunchPeriod+" "+ary[i].end_time);
-            for(int t=0; t<time_slot.length; t++){
-                System.out.print(time_slot[t]);
-            }
-            System.out.println("");
         }
         //printing the lunch time plan
         System.out.println("Target Lunch Partner: ");
         System.out.println(people.toString());
-        System.out.println("Total target: "+people.size());
+        //how much people he can have lunch with
+        System.out.println("Total target: " + people.size());
     }
-    
-    //method to calculate the minute value of student lunch time
-    public int calcMin(int time){
-        int min=time%100;
-        if(time/100==12){
-            min=min+60;
-        }else if(time/100==13){
-            min=min+120;
-        }else if(time/100==14){
-            min=180;
+
+    public int calcMin(int time) {
+        int min = time % 100;
+        if (time / 100 == 12) {
+            min = min + 60;
+        } else if (time / 100 == 13) {
+            min = min + 120;
+        } else if (time / 100 == 14) {
+            min = 180;
         }
         return min;
     }
+
 }
